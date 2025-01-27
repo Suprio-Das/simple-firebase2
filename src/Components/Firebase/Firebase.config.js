cb97bc4d22fc2b09af6cba6e219f5e327de7d651
Suprio-Das <priyamd507@gmail.com>
2025-01-27 23:52:30 +0600

    Firebase config file mistake solved

--- src/Components/Firebase/Firebase.config.js
+++ src/Components/Firebase/Firebase.config.js
@@ -1,4 +1,5 @@
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
     apiKey: "AIzaSyAtFPRNYL8pivyUuVyjTx-Av3I4YRqLBzQ",
@@ -10,4 +11,5 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-export default app;
+const auth = getAuth(app)
+export default auth;
