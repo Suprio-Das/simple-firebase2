d2e1c44b46d4f3e2c2b16839f838b3ec4aa62d81
Suprio-Das <priyamd507@gmail.com>
2025-01-28 13:25:17 +0600

    Simple Password Validation Added

--- src/Components/Register/Register.jsx
+++ src/Components/Register/Register.jsx
@@ -1,14 +1,22 @@
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import auth from "../Firebase/Firebase.config";
+import { useState } from "react";
 
 const Register = () => {
+    const [errorMessage, setErrorMessage] = useState('')
     const handleRegister = (e) => {
         e.preventDefault()
         const name = e.target.name.value
         const email = e.target.email.value
         const password = e.target.password.value
-        const confirmPassword = e.target.confirmPassword.value
-        console.log(name, email, password, confirmPassword)
+        console.log(name, email, password)
+
+        setErrorMessage('');
+
+        if (password.length < 6) {
+            setErrorMessage('Password must be at least 6 characters.')
+            return;
+        }
 
         // creating user with email address and password using firebase
         createUserWithEmailAndPassword(auth, email, password)
@@ -17,6 +25,7 @@ const Register = () => {
             })
             .catch(error => {
                 console.log(error)
+                setErrorMessage(error.message)
             })
     }
     return (
@@ -78,27 +87,15 @@ const Register = () => {
                     />
                 </div>
 
-                <div className="mb-4">
-                    <label
-                        htmlFor="confirmPassword"
-                        className="block text-sm font-medium text-gray-700"
-                    >
-                        Confirm Password
-                    </label>
-                    <input
-                        type="password"
-                        id="confirmPassword"
-                        name="confirmPassword"
-                        className="mt-1 block w-full p-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 border-gray-300"
-                    />
-                </div>
-
                 <button
                     type="submit"
                     className="w-full py-2 px-4 bg-blue-500 text-white font-bold rounded-lg hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 cursor-pointer"
                 >
                     Register
                 </button>
+                {
+                    errorMessage && <p className="text-red-600 my-2 text-center">{errorMessage}</p>
+                }
             </form>
         </div>
     );
